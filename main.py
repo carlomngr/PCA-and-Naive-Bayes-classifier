19e65649c62321190105cd9146d791dc227f0dad
carlomngr@gmail.com <Carletto95!>
2018-12-09 16:36:05 +0100

    PCA done

--- main.py
+++ main.py
@@ -3,30 +3,13 @@ import numpy as np
 import glob
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
-from sklearn import preprocessing
-from scipy import linalg as la
 
-def load_image( infilename ) :
+def load_image (infilename):
 
     img = Image.open(infilename)
     data = np.asarray(img, dtype=np.float64)
     data = data.ravel()
     return data
-
-def myPCA_fit(data, dims_rescaled_data):
-
-    #m, n = data.shape
-    mean = data.mean(axis=0)
-    data -= mean
-    L = np.dot(data, data.T)
-    evals, evecs = la.eigh(L)
-    idx = np.argsort(evals)[::-1]
-    evecs = evecs[:, idx]
-    evals = evals[idx]
-    evecs = evecs[:, :dims_rescaled_data]
-    components = np.dot(data.T, evecs)
-
-    return components.T
 
 def my_trasform(data, X_t, components):
 
@@ -46,54 +29,60 @@ for folder in folders:
 X = np.array(X)
 
 #Standardization of the matrix
-X_std = (X - np.mean(X, axis=0)/np.std(X, axis=0))
+X_std = (X - np.mean(X, axis=0))/(np.std(X, axis=0))
 
-#Compute the PCA I needd
+#Compute the PCA I need
 pca = PCA()
 X_t = pca.fit(X_std)
 pca60 = X_t.components_[0:60, :]
 pca6 = pca60[0:6, :]
 pca2 = pca6[0:2, :]
-
+last6 = X_t.components_[-6:, :]
 
 #Trasform the data
 trasformed2 = my_trasform(X_std, X_t, pca2)
 trasformed6 = my_trasform(X_std, X_t, pca6)
 trasformed60 = my_trasform(X_std, X_t, pca60)
-trasformed602 = X_t.PCA(2)
+trasformedlast6 = my_trasform(X_std, X_t, last6)
+
 #Inverse trasform the data
 trasformed60 = my_inverse_trasform(trasformed60, X_t, pca60)
-#trasformed602 =
+trasformed6 = my_inverse_trasform(trasformed6, X_t, pca6)
+trasformed2 = my_inverse_trasform(trasformed2, X_t, pca2)
+trasformedlast6 = my_inverse_trasform(trasformedlast6, X_t, last6)
+
 #Destandardizise matrix
 trasformed60 = trasformed60 * np.std(X, axis=0) + np.mean(X, axis=0)
-#aaaaaa
+trasformed6 = trasformed6 * np.std(X, axis=0) + np.mean(X, axis=0)
+trasformed2 = trasformed2 * np.std(X, axis=0) + np.mean(X, axis=0)
+trasformedlast6 = trasformedlast6 * np.std(X, axis=0) + np.mean(X, axis=0)
+
 #Plotting reduced images
 img60 = np.reshape(trasformed60[0], (227, 227, 3)).astype(int)
+img6 = np.reshape(trasformed6[0], (227, 227, 3)).astype(int)
+img2 = np.reshape(trasformed2[0], (227, 227, 3)).astype(int)
+imglast6 = np.reshape(trasformedlast6[0], (227, 227, 3)).astype(int)
+img = np.reshape(X[0], (227, 227, 3)).astype(int)
+
+fig = plt.figure()
+columns = 2
+rows = 3
+
+fig.add_subplot(rows, columns, 1).set_title('Real image')
+plt.imshow(img)
+fig.add_subplot(rows, columns, 2).set_title('PCA 60')
 plt.imshow(img60)
+fig.add_subplot(rows, columns, 3).set_title('PCA 6')
+plt.imshow(img6)
+fig.add_subplot(rows, columns, 4).set_title('PCA 2')
+plt.imshow(img2)
+fig.add_subplot(rows, columns, 5).set_title('Last 6 PCA')
+plt.imshow(imglast6)
 plt.show()
 plt.close()
-#matrix, autoval, autovett = myPCA(X_stand, 6)
-#img2 =np.reshape(X[113], (227, 227, 3)).astype(int)
-#plt.imshow(img2, interpolation='nearest')
-#plt.show()
-#X_t = pca2.fit(X_stand)
 
-#X_t = my_trasform(X_stand, comp)
-#X_reproj2 = my_inverse_trasform(X_t, comp)
-#X_new2 = (X_reproj2 * np.std(X, axis=0) + np.mean(X, axis=0))
-#img1 =np.reshape(X_new2[0], (227, 227, 3)).astype(int)
-#plt.imshow(img1, interpolation='nearest')
-#plt.show()
-#plt.close()
 
-#X_reproj = pca2.inverse_transform(X_t)
-#X_new = (X_reproj * np.std(X, axis=0) + np.mean(X, axis=0))
-#img1 =np.reshape(X_new, (227, 227, 3)).astype(int)
-#img = numpy2pil(img1)
 
-#plt.imshow(img1, interpolation='nearest')
-#plt.show()
-#plt.close()
 
 
 
